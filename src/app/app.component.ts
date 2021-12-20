be42fab25525c3d620048c04bbbed5785ba64f60
giriaakula <giriakula3@gmail.com>
2021-12-20 23:49:16 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+
+  constructor() {
+    console.log('giri')
+   }
+  qrResult : string;
+  deviceCurrent: any;
+   cameras:any;
+
+  onCodeResult = (result:string) => {
+    this.qrResult = result
+    console.log(result)
+    window.open(result)
+  }
+
+  onCamerasFound(result:any) {
+    console.log(result)
+    this.cameras = result;
+    this.deviceCurrent = result[0]
+  }
+
+  click1(){
+    this.deviceCurrent = this.cameras[0]
+  }
+
+  click2(){
+    this.deviceCurrent = this.cameras[1]
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ZXingScannerModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
